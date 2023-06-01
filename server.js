b6cd519a2be79706f4ba15864d3a926041a4db75
misselysea <elyseacrawford@gmail.com>
2023-06-01 19:51:20 -0400

    Tip Calculator

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Greeting //
+
 app.get('/greeting', (req, res) => {
     res.send('<h1>Hello, stranger! Elysea here.</h1>');
 });
@@ -9,6 +11,15 @@ app.get('/greeting', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     const name = req.params.name;
     res.send(`<h1>Hello, ${name}! How are you?</h1>`);
+});
+
+// Tip Calculator //
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = parseFloat(req.params.total);
+    const tipPercentage = parseInt(req.params.tipPercentage);
+    const tipAmount = (total * tipPercentage) / 100;
+    res.send(`<h1>The tip amount is ${tipAmount}!</h1>`);
 });
 
 app.listen(port, () => {
