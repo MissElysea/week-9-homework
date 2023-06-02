c9f5f6a2f1e92ba4901dd24b419db785b7463871
misselysea <elyseacrawford@gmail.com>
2023-06-01 20:13:31 -0400

    Magic 8 Ball

--- server.js
+++ server.js
@@ -22,6 +22,24 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`<h1>The tip amount is ${tipAmount}!</h1>`);
 });
 
+// Magic 8 Ball //
+
+const magic8BallResponses = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it, yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+
+app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g, ' ');
+    const randomResponse = magic8BallResponses[Math.floor(Math.random() * magic8BallResponses.length)];
+    const response = `<h1>${question}</h1><h1>${randomResponse}</h1?`;
+    res.send(response);
+});
+
 app.listen(port, () => {
     console.log(`The server is listening on port ${port} gang!`);
 });
